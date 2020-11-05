a3deb8ce2794c7d79c20651a39f818881044d9bc
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-11-05 23:02:30 +0300

    Update for task5 with tests

--- Homework/homework1/task5.py
+++ Homework/homework1/task5.py
@@ -11,8 +11,8 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    if k > len(nums) or not nums or k == 0:
-        return 404
+    if not nums or k == 0:
+        return 0
     max_sum = nums[0]
     for i in range(1, k + 1):
         for j in range(len(nums) - i + 1):
