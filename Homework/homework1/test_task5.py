208aef003714d587a102bc99f5b41fc7278e3213
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-11-06 01:01:09 +0300

    Update names of variables

--- Homework/homework1/test_task5.py
+++ Homework/homework1/test_task5.py
@@ -6,7 +6,7 @@ from homework1.task5 import find_maximal_subarray_sum
 
 
 @pytest.mark.parametrize(
-    ["value1", "value2", "expected_result"],
+    ["nums_list", "k", "expected_result"],
     [
         ([1, 3, -1, -3, 5, 3, 6, 7], 3, 16),
         ([-1, 2, -4, 5, 6, -9, 0], 1, 6),
@@ -36,8 +36,8 @@ from homework1.task5 import find_maximal_subarray_sum
     ],
 )
 def test_find_maximal_subarray_sum(
-    value1: List[int], value2: int, expected_result: int
+    nums_list: List[int], k: int, expected_result: int
 ):
-    actual_result = find_maximal_subarray_sum(value1, value2)
+    actual_result = find_maximal_subarray_sum(nums_list, k)
 
     assert actual_result == expected_result
