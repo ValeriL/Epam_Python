6f44b503ac14cd41690eb1a16fda403e5fddda12
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-11-23 23:31:31 +0300

    Fixed task 1

--- Homework/homework5/test_task1.py
+++ Homework/homework5/test_task1.py
@@ -2,18 +2,14 @@ from homework5.task1 import Homework, Student, Teacher
 
 
 def test_homework_is_active_for_not_expired_homework():
-    hw_text, days = "Any homework", 3
-    not_expired_homework = Homework(hw_text, days)
+    not_expired_homework = Homework("Any homework", 3)
 
     assert not_expired_homework.is_active()
 
 
 def test_do_homework_for_expired_homework(capsys):
-    hw_text, days = "Any homework", 0
-    expired_homework = Homework(hw_text, days)
-
-    first_name, last_name = "Roman", "Petrov"
-    student = Student(first_name, last_name)
+    expired_homework = Homework("Any homework", 0)
+    student = Student("Roman", "Petrov")
 
     student.do_homework(expired_homework)
     captured = capsys.readouterr()
@@ -22,22 +18,20 @@ def test_do_homework_for_expired_homework(capsys):
     assert student.do_homework(expired_homework) is None
 
 
-def test_do_homework_for_not_expired_homework():
-    hw_text, days = "Any homework", 3
-    not_expired_homework = Homework(hw_text, days)
+def test_do_homework_for_not_expired_homework(capsys):
+    not_expired_homework = Homework("Any homework", 3)
+    student = Student("Roman", "Petrov")
 
-    first_name, last_name = "Roman", "Petrov"
-    student = Student(first_name, last_name)
+    student.do_homework(not_expired_homework)
+    captured = capsys.readouterr()
 
+    assert not captured.out
     assert student.do_homework(not_expired_homework) == not_expired_homework
 
 
 def test_create_function():
-    first_name, last_name = "Roman", "Petrov"
-    teacher = Teacher(first_name, last_name)
-
-    hw_text, days = "Any homework", 3
-    created_homework = teacher.create_homework(hw_text, days)
+    teacher = Teacher("Roman", "Petrov")
+    created_homework = teacher.create_homework("Any homework", 3)
 
     assert isinstance(created_homework, Homework)
-    assert created_homework.text == hw_text
+    assert created_homework.text == "Any homework"
