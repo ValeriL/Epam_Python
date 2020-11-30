6a4879bf0ff56678b3924e8aaa32ab6c9d17faf1
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-11-30 22:06:30 +0300

    Fixed task 2

--- Homework/homework6/test_task2.py
+++ Homework/homework6/test_task2.py
@@ -1,32 +1,24 @@
 from homework6.task2 import instances_counter
 
 
-def test_instances_are_counted():
-    @instances_counter
-    class Cls:
-        pass
-
-    Cls()
-
-    assert Cls.instances_counter == 1
-
-
 def test_get_created_instances():
     @instances_counter
     class Cls:
-        pass
+        instances_counter = 42
 
     Cls()
 
     assert Cls.get_created_instances() == 1
+    assert Cls.instances_counter == 42
 
 
 def test_reset_instances_counter():
     @instances_counter
     class Cls:
-        pass
+        instances_counter = 42
 
     Cls()
 
     assert Cls.reset_instances_counter() == 1
     assert Cls.get_created_instances() == 0
+    assert Cls.instances_counter == 42
