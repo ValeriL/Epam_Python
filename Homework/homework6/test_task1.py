4838f5b0b052558e8e97e42bdd41ea2adc90fcfd
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-12-01 18:58:03 +0300

    Fixed test

--- Homework/homework6/test_task1.py
+++ Homework/homework6/test_task1.py
@@ -4,63 +4,69 @@ from homework6.task1 import DeadlineError, Homework, HomeworkResult, Student, Te
 
 import pytest
 
-student = Student("Roman", "Petrov")
-teacher = Teacher("Daniil", "Shadrin")
+
+@pytest.fixture()
+def student():
+    return Student("Roman", "Petrov")
 
 
-def test_homework_result_raise_typeerror():
+@pytest.fixture()
+def teacher():
+    return Teacher("Daniil", "Shadrin")
+
+
+@pytest.fixture()
+def homework():
+    return Homework("Any homework text", 1)
+
+
+def test_homework_result_raise_typeerror(student):
     homework_str = "homework"
     with pytest.raises(TypeError, match="You gave not a Homework object"):
         HomeworkResult(student, homework_str, "solution")
 
 
-def test_do_homework_for_expired_homework():
-    expired_homework = Homework("Any homework", 0)
+def test_do_homework_for_expired_homework(student):
+    expired_homework = Homework("Any homework text", 0)
     with pytest.raises(DeadlineError, match="You are late"):
-        student.do_homework(expired_homework, "Any solution")
+        student.do_homework(expired_homework, "More than 5 symbols solution.")
 
 
-def test_check_incorrect_homework():
-    homework = Homework("Any homework", 1)
+def test_check_incorrect_homework(student, teacher, homework):
     incorrect_result = student.do_homework(homework, "<5")
 
     assert teacher.check_homework(incorrect_result) is False
     assert homework not in Teacher.homework_done
 
 
-def test_check_correct_homework():
+def test_check_correct_homework(student, teacher, homework):
     Teacher.homework_done.clear()
 
-    homework = Homework("HW", 1)
-    correct_result = student.do_homework(homework, "More than 5 symbols.")
+    correct_result = student.do_homework(homework, ">5 symbols solution.")
 
     assert teacher.check_homework(correct_result) is True
     assert Teacher.homework_done == defaultdict(list, {homework: [correct_result]})
 
 
-def test_check_homework_is_without_addition_repeated_solution_into_homework_done_dict():
+def test_not_add_to_homework_done_repeated_solutions(student, teacher, homework):
     Teacher.homework_done.clear()
 
-    teacher1 = Teacher("Daniil", "Shadrin")
-    teacher2 = Teacher("Aleksandr", "Smetanin")
-
-    homework = teacher1.create_homework("HW", 1)
     result = student.do_homework(homework, "Homework is done.")
 
-    teacher1.check_homework(result)
-    teacher2.check_homework(result)
+    teacher.check_homework(result)
+    teacher.check_homework(result)
 
     assert Teacher.homework_done == defaultdict(list, {homework: [result]})
 
 
-def test_reset_homework_results_for_given_homework():
+def test_reset_homework_results_for_given_homework(student, teacher):
     Teacher.homework_done.clear()
 
     homework1 = teacher.create_homework("HW1", 1)
     homework2 = teacher.create_homework("HW2", 1)
 
-    result1 = student.do_homework(homework1, "Any solution.")
-    result2 = student.do_homework(homework2, "Any solution.")
+    result1 = student.do_homework(homework1, ">5 symbols solution.")
+    result2 = student.do_homework(homework2, ">5 symbols solution.")
 
     teacher.check_homework(result1)
     teacher.check_homework(result2)
@@ -70,14 +76,14 @@ def test_reset_homework_results_for_given_homework():
     assert Teacher.homework_done == defaultdict(list, {homework1: [result1]})
 
 
-def test_reset_all_homework_results():
+def test_reset_all_homework_results(student, teacher):
     Teacher.homework_done.clear()
 
     homework1 = teacher.create_homework("HW1", 1)
     homework2 = teacher.create_homework("HW2", 1)
 
-    result1 = student.do_homework(homework1, "Any solution.")
-    result2 = student.do_homework(homework2, "Any solution.")
+    result1 = student.do_homework(homework1, ">5 symbols solution.")
+    result2 = student.do_homework(homework2, ">5 symbols solution.")
 
     teacher.check_homework(result1)
     teacher.check_homework(result2)
