dddebde52b6cc5e519932c30fddb7444334d8d1b
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-12-13 18:01:11 +0300

    fixed test 2

--- Homework/homework9/test_task2.py
+++ Homework/homework9/test_task2.py
@@ -3,9 +3,12 @@ from homework9.task2 import Suppressor, suppressor
 import pytest
 
 
-def test_suppressor_class():
+def test_suppressor_class_given_case():
     with Suppressor(IndexError):
         assert [][2]
+
+
+def test_suppressor_class_subclasses_of_passed_exception():
     with Suppressor(BaseException):
         raise Exception("Hello")
     with Suppressor(BaseException):
@@ -18,9 +21,12 @@ def test_suppressor_class_exception_raised_if_exception_not_in_suppressor():
             5 / 0
 
 
-def test_suppressor_generator():
+def test_suppressor_generator_given_case():
     with suppressor(IndexError):
         assert [][2]
+
+
+def test_suppressor_generator_subclasses_of_passed_exception():
     with suppressor(BaseException):
         raise Exception("Hello")
     with Suppressor(BaseException):
