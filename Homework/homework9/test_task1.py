4d558b530728ef822b95068e641df7f896041172
ValeriL <60858543+ValeriL@users.noreply.github.com>
2020-12-26 15:48:18 +0300

    fixed test

--- Homework/homework9/test_task1.py
+++ Homework/homework9/test_task1.py
@@ -6,16 +6,16 @@ import pytest
 
 
 @pytest.fixture()
-def temp_files(text1: str, text2: str, text3: str):
+def temp_files(text1: tuple, text2: tuple, text3: tuple):
     path1 = "Homework/homework9/test1.txt"
     path2 = "Homework/homework9/test2.txt"
     path3 = "Homework/homework9/test3.txt"
     with open(path1, "w+") as test_file:
-        test_file.write(text1)
+        test_file.writelines(f"{num}\n" for num in text1)
     with open(path2, "w+") as test_file:
-        test_file.write(text2)
+        test_file.writelines(f"{num}\n" for num in text2)
     with open(path3, "w+") as test_file:
-        test_file.write(text3)
+        test_file.writelines(f"{num}\n" for num in text3)
     yield [path1, path2, path3]
     os.remove(path1)
     os.remove(path2)
@@ -25,9 +25,9 @@ def temp_files(text1: str, text2: str, text3: str):
 @pytest.mark.parametrize(
     ["text1", "text2", "text3", "result"],
     [
-        ("1\n3\n5", "2\n7", "4\n6", [1, 2, 3, 4, 5, 6, 7]),
-        ("", "", "", []),
-        ("1\n1", "1\n1\n1", "1", [1, 1, 1, 1, 1, 1]),
+        ((1, 3, 5), (2, 7), (4, 6), [1, 2, 3, 4, 5, 6, 7]),
+        ((), (), (), []),
+        ((1, 1), (1, 1, 1), (1, 1), [1, 1, 1, 1, 1, 1, 1]),
     ],
 )
 def test_merge_sorted_files(temp_files, result):
